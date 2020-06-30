f4c5f6cd1a3df51948e94428c300df828daf4dc8
plebtech <nbmc@protonmail.com>
2020-06-30 17:40:46 -0500

    implemented DOMContentLoaded. wrote toggle function for main page tabs.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,59 @@
-const sfToggle = () => {
-    console.log(this);
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("loading..");
+    tabsListen();
+    onLoad();
+});
+
+const tabsListen = () => {
+    const tabs = document.querySelectorAll('.mp-sub');
+    tabs.forEach(function (tab) {
+        tab.addEventListener('click', nextDivToggle);
+        tab.style.cursor = 'pointer';
+    });
 }
+
+const nextDivToggle = (e) => {
+    const nt = e.target.nextElementSibling;
+    const nts = getComputedStyle(nt).getPropertyValue('display');
+    if (nts === 'block') {
+        nt.style.display = 'none';
+    } else {
+        nt.style.display = 'block';
+    }
+}
+
+const onLoad = () => {
+    console.log("loaded.");
+}
+
+function sfToggle() {
+    if (screenfetch.style.display == 'block') {
+        screenfetch.style.display = 'none';
+    }
+    else {
+        screenfetch.style.display = 'block';
+    }
+}
+
+
+
+// document.addEventListener("DOMContentLoaded", (event) => {
+//     console.log("loading...");
+
+
+
+//     const toggleTabs = document.querySelectorAll('.mp-sub');
+//     toggleTabs.forEach( function (tab) {
+//         tab.addEventListener("click", tabClicked);
+//     });
+
+//     const tabClicked = (e) => {
+//         console.log(e);
+//     }
+
+//     const sfToggle = () => {
+//         console.log(this);
+//     }
+
+//     console.log("loaded.");
+// });
