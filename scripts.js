61b0a928cdc3fbe60a16a6eedf11d20a07d942e6
plebtech <nbmc@protonmail.com>
2021-02-21 20:28:10 -0600

    overhauled front page. beginning animation of title.

--- scripts.js
+++ scripts.js
@@ -2,9 +2,26 @@
 // 'boot loader' of script.
 document.addEventListener("DOMContentLoaded", () => {
     console.log("loading..");
+    titleShuffle();
     tabsListen();
     onLoad();
 });
+
+const timer = ms => new Promise(res => setTimeout(res, ms));
+
+const titleShuffle= async () => {
+    const title = document.getElementById('slot-machine');
+    const titleText = title.textContent;
+    title.textContent = '';
+    for (i = 0; i < titleText.length; i++) {
+        // if (titleText[i] === ' ') {
+        //     continue;
+        // } else {
+            title.innerHTML += titleText[i];
+            await timer(200);
+        // }
+    }
+}
 
 // adds click listener for every instance of .mp-sub class.
 const tabsListen = () => {
@@ -24,6 +41,8 @@ const nextDivToggle = (e) => {
         nt.style.display = 'block';
     }
 }
+
+
 
 const onLoad = () => {
     console.log("loaded.");
