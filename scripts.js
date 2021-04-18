75bb06219ef8e07c5eeaad6fbc4cb0cd21dc03c2
plebtech <nbmc@protonmail.com>
2021-04-18 16:24:00 -0500

    several updates; finished animating title with vanilla js.

--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
 // DOMContentLoaded required with scripts loaded in <head> of html.
-// 'boot loader' of script.
+// 'boot loader' of script (hoisting).
 document.addEventListener("DOMContentLoaded", () => {
     console.log("loading..");
-    titleShuffle();
+    titleAnimate();
     tabsListen();
     onLoad();
 });
@@ -11,20 +11,64 @@ const gradientDown = "linear-gradient(180deg, #002b36, #eee8d5) 10";
 const gradientUp = "linear-gradient(0deg, #002b36, #eee8d5) 10";
 const gradientNone = "#073642";
 
+// basic timer w/ promise using setTimeout function.
 const timer = ms => new Promise(res => setTimeout(res, ms));
 
-const titleShuffle= async () => {
+// animation on page's title.
+const titleAnimate = async () => {
     const title = document.getElementById('slot-machine');
     const titleText = title.textContent;
     title.textContent = '';
-    for (i = 0; i < titleText.length; i++) {
-        // if (titleText[i] === ' ') {
-        //     continue;
-        // } else {
+    const tTL = titleText.length;
+    for (i = 0; i < tTL; i++) {
+        if (titleText[i] === ' ') {
             title.innerHTML += titleText[i];
-            await timer(200);
-        // }
+        } else {
+            title.innerHTML += titleText[i];
+            let delay = randomNumber(25, 250);
+            await timer(delay);
+        }
     }
+    let blinkChar;
+    let blinkInterval;
+    let target;
+    while (true) {
+        // randomly sets the targeted character to blink.
+        blinkChar = randomNumber(0, tTL - 1);
+        target = title.textContent[blinkChar];
+        // randomly sets an interval between blinks.
+        blinkInterval = randomNumber(100, 1000);
+        // if the character is a space, no wait.
+        if ((target) === ' ') {
+            blinkInterval = 0;
+        }
+        title.innerHTML = applySpan(titleText, blinkChar, "offBlink");
+        await (timer(blinkInterval));
+        title.innerHTML = applySpan(titleText, blinkChar, "overBlink");
+        await (timer(200));
+        title.textContent = titleText;
+        await (timer(blinkInterval * 2));
+    }
+}
+
+// returns a text string with a single recolored character (at position pos, recolored color via span tag).
+const applySpan = (text, pos, cssClass) => {
+    let result = '';
+    for (i = 0; i < text.length; i++) {
+        if (i === pos) {
+            result += `<span class="${cssClass}">${text[i]}</span>`;
+        } else {
+            result += text[i];
+        }
+    }
+    // console.log(result);
+    return result;
+}
+
+const randomNumber = (min, max) => {
+    let val = Math.floor(Math.random() * ((max + 1) - min) + min);
+    // console.log(val);
+    return val;
 }
 
 // adds click listener for every instance of .mp-sub class.
